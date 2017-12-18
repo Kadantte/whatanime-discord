c9bbfcc23939414c4a683e1a43895e4ccb535a6e
StandB <standb@protonmail.com>
2017-12-18 15:55:22 +0100

    refactor to async/await

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-var base64 = require('node-base64-image');
-var request = require('request');
 const Eris = require("eris");
 const config = require("./config.json");
-var popura =  require('popura');
 
-const mal = popura(config.mal_name, config.mal_password);
-
-const wa_url = `https://whatanime.ga/api/search?token=${config.wa_token}`
-
-// TODO: make catch less ugly and make github ready
-
-class Anime {
-    constructor(title_romaji, title_english, title_japanese, episode) {
-        this.title_romaji = title_romaji;
-        this.title_english = title_english;
-        this.title_japanese = title_japanese;
-        this.episode = episode;
-    }
-}
+// whatanime functionality
+const wa = require('./wa.js')
 
 var bot = new Eris.CommandClient(config.d_token, {}, {
     description: "A bot to find out what anime an image is from",
@@ -31,80 +16,36 @@ bot.on("ready", () => {
 
 bot.registerCommand("ping", "pong", {description: "Test command"});
 
-bot.registerCommand("whatanime", (msg, args) => {
+bot.registerCommand("whatanime", async (msg, args) => {
     if (msg.attachments.length == 0) { return; }
     var image = msg.attachments[0].url;
 
-    return new Promise((resolve, reject) => {
-        var options = {string: true};
-        base64.encode(image, options, (err, image) => {
-            if (err) reject(err);
-            resolve(image);
-        });
-    })
-    
-    .then(b64 => {
-        // get json from whatanime.ga api using base 64 string of image
-        return new Promise((resolve, reject) => {
-            request.post({url: wa_url, form: {image: b64}}, function(err,httpResponse,body) {
-                if (err) reject(err)
-                resolve(body)
-            });
-        })
-    })
-    
-    .then(resp => {
-        // get data from json and send embed
-        return new Promise((resolve, reject) => {
-            data = JSON.parse(resp)["docs"][0];
-            var anime = new Anime(data["title_romaji"], data["title_english"], data["title"], data["episode"]);
-            resolve(anime)
-        })
-    })
+    const b64 = await wa.imgtob64(image)
+    const json = await wa.callapi(b64)
+    const anime = await wa.parsejson(json)
+    const mal_link = await wa.getmallink(anime)
 
-    .then(anime => {
-        // get MAL link from API using popura
-        return new Promise((resolve, reject) => {
-            mal.searchAnimes(anime.title_english).then(res => {
-                var mal_link = `https://myanimelist.net/anime/${res[0]['id']}`
-                resolve(anime, mal_link)
-            }).catch(err => {
-                reject(err)
-            })
-        })
+    bot.createMessage(msg.channel.id, {
+        embed: {
+            author: {
+                name: msg.author.username,
+                icon_url: msg.author.avatarURL
+            },
+            url: mal_link,
+            title: anime.title_romaji,
+            color: 0xFF0000,
+            fields: [
+                { name: "Romaji title", value: anime.title_romaji, inline: true },
+                { name: "Japanese title", value: anime.title_japanese, inline: false },
+                { name: "Episode", value: anime.episode, inline: false },
+            ],
+            footer: { 
+                text: "Anime found with whatanime.ga",
+                icon_url: "https://whatanime.ga/favicon.png"
+            },
+            thumbnail: { url: image }
+        }
     })
-
-    .then((anime, mal_link) => {
-        bot.createMessage(msg.channel.id, {
-            embed: {
-                author: {
-                    name: msg.author.username,
-                    icon_url: msg.author.avatarURL
-                },
-                url: mal_link,
-                title: anime.title_romaji,
-                color: 0xFF0000,
-                fields: [
-                    { name: "Romaji title", value: anime.title_romaji, inline: true },
-                    { name: "Japanese title", value: anime.title_japanese, inline: false },
-                    { name: "Episode", value: anime.episode, inline: false },
-                ],
-                footer: { 
-                    text: "Anime found with whatanime.ga",
-                    icon_url: "https://whatanime.ga/favicon.png"
-                },
-                thumbnail: { url: image }
-            }
-        })
-    })
-
-    // crappy error handling
-    .catch((err) => {
-        console.log(err);
-        bot.createMessage(msg.channel.id, "Something went wrong, try again later.")
-        return
-    })
-
 }, {
     // command info
     description: "Find the anime of the provided picture (attachment).",
---
+++ wa.js
@@ -0,0 +1,35 @@
+const fetch = require('fetch-base64');
+const rp = require('request-promise');
+const popura = require('popura');
+
+const config = require("./config.json");
+const wa_url = `https://whatanime.ga/api/search?token=${config.wa_token}`
+const mal = popura(config.mal_name, config.mal_password);
+
+const Anime = (title_romaji, title_english, title_japanese, episode) => {
+    return { title_romaji, title_english, title_japanese, episode }
+}
+
+exports.imgtob64 = async function(image_url) {
+    const res = await fetch.remote(image_url)
+    return res[0];
+}
+
+exports.callapi = async function(b64) {
+    var options = {
+        method: 'POST',
+        uri: wa_url,
+        form: { image: b64 },
+    };
+    return await rp(options)
+}
+
+exports.parsejson = async function(json) {
+    data = JSON.parse(json)["docs"][0];
+    return Anime(data["title_romaji"], data["title_english"], data["title"], data["episode"]);
+}
+
+exports.getmallink = async function(anime) {
+    res = await mal.searchAnimes(anime.title_english)
+    return `https://myanimelist.net/anime/${res[0]['id']}`
+}
